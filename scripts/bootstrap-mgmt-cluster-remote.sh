eb8c8ff75af7710e3d7eec0e8b1f218d79d7e256
Juan Roldán <juanroldan1989@gmail.com>
2025-06-09 17:49:32 +0200

    ESO (External Secrets Operator) provision within clusters
    - CLOUDFLARE_API_TOKEN access setup in mgmt-cluster/london/newyork via ESO

--- scripts/bootstrap-mgmt-cluster-remote.sh
+++ scripts/bootstrap-mgmt-cluster-remote.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Customize these variables
+AWS_PROFILE="default"
 CLUSTER_NAME="mgmt-cluster"
 NODE_COUNT=1
 NODE_SIZE="g4s.kube.large"
@@ -27,6 +28,24 @@ kubectl config use-context mgmt-cluster
 
 echo "🔐 Injecting Sealed Secrets public key..."
 kubectl apply -f .sealed-secrets/mgmt/sealed-secrets-key.yaml -n kube-system
+
+echo "🔍 Checking AWS profile: ${AWS_PROFILE}"
+ACCESS_KEY=$(aws configure get aws_access_key_id --profile "${AWS_PROFILE}")
+SECRET_KEY=$(aws configure get aws_secret_access_key --profile "${AWS_PROFILE}")
+
+if [[ -z "$ACCESS_KEY" || -z "$SECRET_KEY" ]]; then
+  echo "❌ AWS credentials not found in profile '${AWS_PROFILE}'"
+  exit 1
+fi
+echo "✅ AWS credentials loaded from profile '${AWS_PROFILE}'"
+
+echo "🔐 Creating AWS secret for ESO (External Secrets Operator)"
+kubectl create ns external-secrets || true
+kubectl create secret generic aws-creds \
+  --namespace external-secrets \
+  --from-literal=access-key="$ACCESS_KEY" \
+  --from-literal=secret-access-key="$SECRET_KEY" \
+  --dry-run=client -o yaml | kubectl apply -f -
 
 echo "🔄 Provisioning mgmt-cluster with essentials ..."
 kubectl apply -f manifests/bootstrap/mgmt-cluster.yaml
