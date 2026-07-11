526337295add8c36779aff98cc04b3edabe5ca50
Juan Roldán <juanroldan1989@gmail.com>
2026-07-11 09:26:01 +0200

    hetzner module added - nuremberg cluster as another alternative

--- scripts/seal-mgmt-secrets.sh
+++ scripts/seal-mgmt-secrets.sh
@@ -10,6 +10,7 @@ OUTPUT_FILE="bootstrap/crossplane/0-crossplane-sealed-secrets.yaml"
 # --- Ensure required environment variables are set ---
 : "${CIVO_TOKEN:?CIVO_TOKEN is required}"
 : "${VULTR_TOKEN:?VULTR_TOKEN is required}"
+: "${HETZNER_TOKEN:?HETZNER_TOKEN is required}"
 : "${CLOUDFLARE_API_TOKEN:?CLOUDFLARE_API_TOKEN is required}"
 
 # --- Create the base secret YAML with annotations and type ---
@@ -17,6 +18,8 @@ kubectl create secret generic "${SECRET_NAME}" \
   --namespace "${NAMESPACE}" \
   --from-literal=CIVO_TOKEN="${CIVO_TOKEN}" \
   --from-literal=TF_VAR_civo_token="${CIVO_TOKEN}" \
+  --from-literal=HETZNER_TOKEN="${HETZNER_TOKEN}" \
+  --from-literal=TF_VAR_hetzner_token="${HETZNER_TOKEN}" \
   --from-literal=VULTR_TOKEN="${VULTR_TOKEN}" \
   --from-literal=TF_VAR_vultr_token="${VULTR_TOKEN}" \
   --from-literal=TF_VAR_cloudflare_api_token="${CLOUDFLARE_API_TOKEN}" \
